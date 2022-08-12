19156d48805aa6fd9cb2fca8884eaf3905e3f3b0
gayun82 <gayun0820@naver.com>
2022-08-12 17:03:03 +0900

    ddddd

---
+++ web/src/test/java/co/yedam/web/DaoTest.java
@@ -0,0 +1,16 @@
+package co.yedam.web;
+//junit 단위체스트
+
+import org.junit.Test;
+
+public class DaoTest {
+	@Test
+	public void test1() {
+		System.out.println("aa");
+	}
+	@Test
+	public void test2() {
+		//int a=5/0;
+		System.out.println("bb");
+	}
+}
